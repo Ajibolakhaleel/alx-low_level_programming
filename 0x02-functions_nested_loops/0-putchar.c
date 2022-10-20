39b660849839400e6b44ba93487afc1be93c8399
Ajibolakhaleel <k.ajibola@alustudent.com>
2022-10-19 17:12:08 -0700

    5

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  * main - check description
+ *   * description: prints _putchar, followed by a line.
+ *    *  Return: (Success 0)
+ *     */
+int main(void)
+{
+		char word[8] = "_putchar";
+			int i;
+
+				for (i = 0; i < 8; i++)
+							_putchar(word[i]);
+					_putchar('\n');
+
+						return (0);
+}
